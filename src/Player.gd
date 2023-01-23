1620cc6ead4f3dd49a0a85a55809f706b52cf3d3
cannnAvar <72388136+cannnAvar@users.noreply.github.com>
2023-01-23 21:53:20 +0300

    Changes to be committed: 	new file:   Actors/Player.tscn 	new file:   Levels/LevelTamplate.tscn 	new file:   default_env.tres 	new file:   icon.png 	new file:   icon.png.import 	new file:   project.godot 	new file:   src/Player.gd

---
+++ src/Player.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+const UP = Vector2(0.0, -1.0)
+var Maxspeed = 80
+var MaxFallsSpeed = 200
+var Gravity = 30
+var jumpForce = 300
+export var accel = 10
+
+var motion = Vector2()
+
+func _physics_process(delta):
+	
+	motion.y += Gravity
+	if motion.y > MaxFallsSpeed:
+		motion.y = MaxFallsSpeed
+	
+	motion.x = clamp(motion.x, -Maxspeed, Maxspeed)
+	
+	if Input.is_action_pressed("Right"):
+		motion.x += accel
+
+	elif Input.is_action_pressed("Left"):
+		motion.x -= accel
+
+	else:
+		motion.x = lerp(motion.x, 0, 0.2)
+
+
+	if is_on_floor():
+		if Input.is_action_just_pressed("Jump"):
+			motion.y = -jumpForce
+	
+	motion = move_and_slide(motion, UP)
